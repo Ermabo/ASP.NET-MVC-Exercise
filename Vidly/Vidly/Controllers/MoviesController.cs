dee223f705cfff9249393b4175efbec37e241483
Marcus Bogren <marcus.bogren@gmail.com>
2017-05-18 19:07:10 +0200

    Added several migrations and classes

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+using System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Movies/random
+        public ActionResult Random()
+        {
+            
+            var movie = new Movie() {Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+
+            };
+
+            var viewModel = new RandomMovieVewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content($"id= {id}");
+        }
+
+        //movies
+        //[Route("movies")]
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+
+            return View(movie);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie() {Id = 1, Name = "Shrek" },
+                new Movie() {Id = 2, Name = "Wall-e" }
+            };
+        }
+    }
+}
