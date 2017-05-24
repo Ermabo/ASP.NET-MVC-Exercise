bef2f52371b72e34fbc8c221fccdb1ef02d7816c
Marcus Bogren <marcus.bogren@gmail.com>
2017-05-24 15:08:51 +0200

    Cleaned up the code

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -64,34 +64,7 @@ namespace Vidly.Controllers
                 movieInDb.ReleaseDate = movie.ReleaseDate;
             }
 
-            try
-            {
-                _context.SaveChanges();
-            }
-            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
-            {
-                Exception raise = dbEx;
-                foreach (var validationErrors in dbEx.EntityValidationErrors)
-                {
-                    foreach (var validationError in validationErrors.ValidationErrors)
-                    {
-                        string message = string.Format("{0}:{1}",
-                            validationErrors.Entry.Entity.ToString(),
-                            validationError.ErrorMessage);
-                        // raise a new exception nesting  
-                        // the current instance as InnerException  
-                        raise = new InvalidOperationException(message, raise);
-                    }
-                }
-
-            //    catch(System.Data.up)
-            //{
-
-            //}
-                throw raise;
-            }
-
-            
+            _context.SaveChanges();
 
             return RedirectToAction("Index", "Movies");
         }
