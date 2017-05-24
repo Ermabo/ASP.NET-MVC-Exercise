1d62c6580996e1d5af2dd18fb265f46d35295d8b
Marcus Bogren <marcus.bogren@gmail.com>
2017-05-24 14:30:43 +0200

    Forgot to add everything..

---
+++ Vidly/Vidly/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        [Display(Name = "Number in Stock")]
+        public short? NumberInStock { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte? GenreId { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                return Id != 0 ? "Edit Movie" : "New movie";
+            }
+        }
+
+        public MovieFormViewModel()
+        {
+            Id = 0; 
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            NumberInStock = movie.NumberInStock;
+            GenreId = movie.GenreId;
+            //DateAdded = movie.DateAdded;
+        }
+    }
+
+}
